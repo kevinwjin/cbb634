2cc54f124a5182a0b53d5777a057fcedc715cc00
kevinwjin <kevin.jin@utdallas.edu>
2023-09-21 19:35:15 -0400

    Use script instead of notebook

---
+++ pset_1/exercise_5.py
@@ -0,0 +1,30 @@
+# Exercise 5: Analysis of MeSH data
+
+# Download and parse XML
+import xml.etree.ElementTree as ET
+
+print("Parsing XML...")
+mesh = ET.parse("/Users/kevin/Documents/Computational Methods for Informatics/cbb634/pset_1/desc2023.xml")
+print("Parsing complete.")
+
+root = mesh.getroot()
+print(root.tag, root.attrib)
+
+# Extract values of interest
+
+# Biomedical explanation:
+
+# Generic function versions
+def get_name(ui):
+    pass
+
+
+def get_ui(name):
+    pass
+
+
+def get_descendent_names(ui, name):
+    pass
+
+
+# Test generic function versions
