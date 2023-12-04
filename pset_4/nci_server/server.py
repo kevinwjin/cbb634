4d97de0ba10cc50192443d292e0ea7a78b9ffb09
kevinwjin <kevin.jin@utdallas.edu>
2023-12-04 13:39:51 -0500

    Add comments

--- pset_4/nci_server/server.py
+++ pset_4/nci_server/server.py
@@ -4,11 +4,12 @@ from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
 
+# Main/index page with form
 @app.route("/")
 def index():
     return render_template("index.html")
 
-
+# Info page with results
 @app.route("/info", methods=["POST"])
 def info():
     state = request.form['state'].lower()
@@ -16,11 +17,13 @@ def info():
     return render_template("info.html", state=state, analysis=analysis)
 
 
+# API endpoint for state
 @app.route("/state/<string:name>")
 def state(name):
     data = {"names": ["John", "Jacob", "Julie", "Jennifer"]}
     return jsonify(data)
 
 
+# Prevents execution if imported
 if __name__ == '__main__':
     app.run(debug=True, ssl_context=("cert.pem", "key.pem"), port=5000)
