d95ef91e972d3a17065b17c0e7e5c06ae05d99af
kevinwjin <kevin.defan@gmail.com>
2023-12-17 16:23:30 -0500

    Final project report and server

--- final_project/server.py
+++ final_project/server.py
@@ -1,4 +1,5 @@
-# Implement an NCI cancer incidence lookup API as a web server in Flask
+# Server backend for final project
+# Author: Kevin Jin
 from flask import Flask, render_template, request, jsonify
 import pandas as pd
 
@@ -14,34 +15,13 @@ def index():
 # API endpoint for state
 @app.route("/state/<string:name>")
 def state(name):
-    data = pd.read_csv("incidence_rates.csv")
-    if name == "US" or name == "U.S." or name == "United States":
-        name = "US"
-    elif name == "DC" or name == "D.C." or name == "Washington DC" or name == "Washington D.C.":
-        name = "District of Columbia"
-    elif name not in data["State"]:
-        return render_template("error.html")
-    else:
-        name.capitalize()
-    entry = dict(data[data["State"].str.contains(name)].iloc[0, [1, 2]])
-    return jsonify(entry)
+    pass
 
 
 # Info page with results
-@app.route("/info/", methods=["GET"])
+@app.route("/info/", methods=["POST"])
 def info():
-    state = request.args.get("state")
-    data = pd.read_csv("incidence_rates.csv")
-    if state == "U.S." or state == "United States":
-        state = "US"
-    elif state == "DC" or state == "D.C." or state == "Washington DC" or state == "Washington D.C.":
-        state = "District of Columbia"
-    elif state not in data["State"]:
-        return render_template("error.html")
-    else:
-        state.capitalize()
-    rate = float(data[data["State"].str.contains(state)].iloc[0, 2])
-    return render_template("info.html", state=state, rate=rate)
+    pass
 
 
 # Prevents execution if imported
